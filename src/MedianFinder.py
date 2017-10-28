efb885db3aa268454aabd5879124ae23fc5b6086
euo486 <shiyan.cao@capitalone.com>
2017-10-27 23:32:01 -0400

    Created Solution

--- src/MedianFinder.py
+++ src/MedianFinder.py
@@ -5,12 +5,14 @@ from heapq import *
 class MedianFinder:
   def __init__(self):
     self.small, self.large = [], []
+    self.total = 0
 
   def addNum(self, num):
     heappush(self.large, num)
     heappush(self.small, -heappop(self.large))
     if len(self.large) < len(self.small):
       heappush(self.large, -heappop(self.small))
+    self.total += num
 
   def getMedian(self):
     if not self.large: return 0
@@ -18,3 +20,9 @@ class MedianFinder:
       return self.large[0] 
     else:
       return (self.large[0] - self.small[0]) / 2.0
+
+  def getSize(self):
+    return len(self.small) + len(self.large)
+
+  def getTotal(self):
+    return self.total
--- src/find_political_donors.py
+++ src/find_political_donors.py
@@ -6,28 +6,13 @@
 # P15 NUM(14,2), * `TRANSACTION_AMT`: amount of the transaction
 # P16 VC(9), * `OTHER_ID`: a field that denotes whether contribution came from a person or an entity
 
-from heapq import *
-
-class MedianHelper:
-  def __init__():
-    self.small, self.large = [], []
-    pass
-
-  def addNum(num):
-    heappush(self.large, num)
-    heappush(self.small, num)
-    pass
-
-  def findMedian():
-    return 
-      
-
-  
+from MedianFinder import MedianFinder
 
 if __name__ == "__main__":
+  dic, dic2, sortKeys = {}, {}, []
   with open("../input/itcont.txt") as f:
     for line in f:
-      print(line)
+#      print(line)
       linelist = line.split("|")
 
       # Ignore lines that not good
@@ -40,8 +25,33 @@ if __name__ == "__main__":
       transaction_amt = linelist[14]
       other_id = linelist[15]
 
-      print(cmte_id, zip_code5, transaction_dt, transaction_amt, other_id)
-      if zip_code5 not in dic:
-        dic
-      
-      
+      # For Output 2
+      key = cmte_id + "_" + transaction_dt
+      if key not in dic2:
+        sortKeys.append([cmte_id, transaction_dt[4:] + transaction_dt[:4]])
+        mh2 = MedianFinder()
+      else:
+        mh2 = dic2[key]
+      dic2[key] = mh2
+      mh2.addNum(float(transaction_amt))
+      #for k1,v1 in dic2.iteritems():
+      #  print(k1, v1.large, v1.small)
+      #print("______________")
+
+      # For Output 1
+      #print(cmte_id, zip_code5, transaction_dt, transaction_amt, other_id)
+      mh = dic.get(zip_code5, MedianFinder())
+      dic[zip_code5] = mh
+      mh.addNum(float(transaction_amt))
+      #print(dic)
+
+      # 1st Output
+      print("|".join([cmte_id, zip_code5, str(int(round(mh.getMedian()))), str(mh.getSize()), str(int(round(mh.getTotal())))]))
+
+    print("-------------------")
+    # 2nd Output
+    #print(sortKeys)
+    for row in sorted(sortKeys, key = lambda x: (x[0], x[1])):
+      k = row[0] + "_" + row[1][4:] + row[1][:4]
+      tmp = [row[0], row[1][4:] + row[1][:4], str(int(round(dic2[k].getMedian()))), str(dic2[k].getSize()), str(int(round(dic2[k].getTotal())))]
+      print("|".join(tmp))
